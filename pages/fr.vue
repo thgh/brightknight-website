bc30b00d8c8db1b90d74e97e8c10e7e1809d88f1
Thomas Ghysels <info@thomasg.be>
2017-12-18 09:44:06 +0100

    Add netlify cms

---
+++ pages/fr.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="content-container" v-if="page">
+    <block v-for="block in page.blocks" :data="block" :key="block['@id']" />
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({ page: {} }),
+  asyncData: async ({ app, params, payload }) => {
+    return {
+      page: await app.$content('/').get('fr') || payload
+    }
+  },
+  head () {
+    return {
+      title: this.page.title
+    }
+  }
+}
+</script>
---
+++ pages/nl.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="content-container" v-if="page">
+    <block v-for="block in page.blocks" :data="block" :key="block['@id']" />
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({ page: {} }),
+  asyncData: async ({ app, params, payload }) => {
+    return {
+      page: await app.$content('/').get('nl') || payload
+    }
+  },
+  head () {
+    return {
+      title: this.page.title
+    }
+  }
+}
+</script>
